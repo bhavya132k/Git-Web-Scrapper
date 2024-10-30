6ee1d5b0ceffef5f6c19d5b652cc392073bd76a4
rapo7 <144160473+rapo7@users.noreply.github.com>
2024-10-30 14:05:09 -0400

    ticket 3.7

--- frontend/src/components/RepoTable.tsx
+++ frontend/src/components/RepoTable.tsx
@@ -3,7 +3,7 @@ import Button from "@mui/material/Button";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import { useNavigate } from "react-router-dom"; // useNavigate in the parent component
 import data from "../data/dummy.json";
-import { Chip, Link } from "@mui/material";
+import { Chip, Link, Tooltip, Typography } from "@mui/material";
 
 export default function RepoTable() {
   const navigate = useNavigate(); // Hook should be used here, in the parent component
@@ -13,12 +13,13 @@ export default function RepoTable() {
       field: "id",
       headerName: "ID",
       width: 70,
-      hideable:true
+      hideable: true,
     },
     {
       field: "full_name",
       headerName: "Repo Name",
       width: 250,
+      type: "string",
       renderCell: (params) => (
         <Link href={params.row.html_url} target="_blank">
           {params.row.full_name}
@@ -26,20 +27,29 @@ export default function RepoTable() {
       ),
     },
     {
-      field: "stargazers_count",
-      headerName: "stars",
-      width: 160,
+      field: "description",
+      headerName: "Description",
+      type: "string",
+      width: 300,
+      renderCell: (params) => (
+        <Tooltip title={params.row.description ? params.row.description : "N/A"}>
+          <Typography variant="body2" noWrap>
+            {params.row.description ? params.row.description : "N/A"}
+          </Typography>
+        </Tooltip>
+      ),
     },
     {
-      field: "forks_count",
-      headerName: "Forks",
+      field: "stargazers_count",
+      headerName: "stars",
       type: "number",
-      width: 160,
+      width: 40,
     },
     {
       field: "owner.login",
       headerName: "Origin and Pedigree",
       width: 160,
+      sortable: false,
       renderCell: (params) => (
         <Chip label={params.row.owner.login ? params.row.owner.login : "N/A"} />
       ),
@@ -53,8 +63,15 @@ export default function RepoTable() {
       field: "license.name",
       headerName: "License",
       width: 200,
+      sortable: false,
       renderCell: (params) => (
-        <Chip label={params.row.license ? (params.row.license.name || "No License") : "No License"} />
+        <Chip
+          label={
+            params.row.license
+              ? params.row.license.name || "No License"
+              : "No License"
+          }
+        />
       ),
     },
 
@@ -64,6 +81,7 @@ export default function RepoTable() {
       width: 200,
       sortable: false,
       filterable: false,
+      type: "actions",
       renderCell: (params) => (
         <Button
           variant="contained"
