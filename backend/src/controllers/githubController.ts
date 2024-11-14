faf5dc397d7f9399b9abe2ae600c60f3fc9d30ed
rapo7 <144160473+rapo7@users.noreply.github.com>
2024-11-14 15:30:20 -0500

    backend sorting based on last_updated

--- backend/src/controllers/githubController.ts
+++ backend/src/controllers/githubController.ts
@@ -18,7 +18,7 @@ export const getRepos = async (req: Request, res: Response) => {
         const query = keywordsArray.map(keyword => `${keyword}`).join('+');
 
         // const built_URL = `https://api.github.com/search/repositories?q=(${query}+language:C&per_page=25&page=${page}`;
-        const built_URL = `https://api.github.com/search/repositories?q=${query}+language:C&per_page=25&page=${page}`;
+        const built_URL = `https://api.github.com/search/repositories?q=${query}+language:C&per_page=25&page=${page}&sort=updated&order=desc`;
 
         let response = await axios.get(built_URL, {
             headers: {
@@ -30,7 +30,7 @@ export const getRepos = async (req: Request, res: Response) => {
 
         if (response.data.total_count < 25) {
 
-            const built_URL = `https://api.github.com/search/repositories?q=${query}+language:C`;
+            const built_URL = `https://api.github.com/search/repositories?q=${query}+language:C&sort=updated&order=desc`;
             // wait for 1 second before making the request
             await new Promise(resolve => setTimeout(resolve, 1000));
             response = await axios.get(built_URL, {
@@ -54,6 +54,27 @@ export const getRepos = async (req: Request, res: Response) => {
         res.status(404).json({ error: 'System is offline Check your internet' });
     }
 }
+
+export const getDependencies = async (req: Request, res: Response) => {
+
+    const owner = req.query.owner;
+    const repo = req.query.repo;
+    if (typeof owner !== 'string' || typeof repo !== 'string') {
+        return res.status(400).json({ error: 'Invalid owner or repo' });
+    }
+    try {
+        const response = await axios.get(`https://api.github.com/repos/${owner}/${repo}/dependency-graph/sbom`, {
+            headers: {
+                Authorization: `Bearer ${GITHUB_API_TOKEN}`,
+            },
+        });
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(404).json({ error: 'System is offline Check your internet' });
+    }
+}
+
 
 
 
