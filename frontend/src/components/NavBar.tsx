53ac70c079af32ee0d8462ee07dc2838bb2224a2
rapo7 <144160473+rapo7@users.noreply.github.com>
2024-11-18 18:45:28 -0500

    back button

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -4,12 +4,16 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 export default function NavBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" >
         <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="Back" sx={{ mr: 2 }} onClick={() => window.history.back()}>
+            <ArrowBackIcon />
+          </IconButton>
           <IconButton
             size="large"
             edge="start"
